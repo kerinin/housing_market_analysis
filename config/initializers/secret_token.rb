51989b771496fc8599d6c10af3ba5fffdd232c8c
Ryan Michael <kerinin@gmail.com>
2012-12-20 08:34:54 -0600

    rails!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HousingMarketAnalysis::Application.config.secret_token = '69b26d4b12b2a40aec4039cd8360e472b50ecd82b7c077b16eeee2b67fab012ebc4f40f710286952371c2b7abf0c77e96fac833457e5f09d054c5bb7edf24e31'
